6599a4988c63bc2ec41274ddf991060f54aa7b17
IT21375132 <gokulabisheak12@gmail.com>
2023-05-06 22:41:48 +0530

    Sign Up

---
+++ app/src/main/java/com/example/tourism_app/activities/LoginActivity.kt
@@ -0,0 +1,13 @@
+package com.example.tourism_app.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.tourism_app.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.login)
+    }
+}
---
+++ app/src/main/java/com/example/tourism_app/activities/SignUpActivity.kt
@@ -0,0 +1,14 @@
+package com.example.tourism_app.activities
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.tourism_app.R
+
+class SignUpActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.sign_up)
+    }
+}

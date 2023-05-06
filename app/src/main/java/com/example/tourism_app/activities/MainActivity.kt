55431126029dfd3f0a556dedb7dfb41873ee1c3b
IT21375132 <gokulabisheak12@gmail.com>
2023-05-06 22:42:17 +0530

    Login

--- app/src/main/java/com/example/tourism_app/activities/MainActivity.kt
+++ app/src/main/java/com/example/tourism_app/activities/MainActivity.kt
@@ -12,12 +12,16 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var redirectAddDestinationBtn: Button
     private lateinit var redirectFetchDestinationBtn: Button
+    private lateinit var redirectLoginBtn: Button
+    private lateinit var redirectSignUpBtn: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         redirectAddDestinationBtn = findViewById(R.id.redirect_add_destination_button)
         redirectFetchDestinationBtn = findViewById(R.id.redirect_fetch_destination_button)
+        redirectLoginBtn = findViewById(R.id.redirect_login_button)
+        redirectSignUpBtn = findViewById(R.id.redirect_signup_button)
 
         var firebase: DatabaseReference = FirebaseDatabase.getInstance().getReference()
 
@@ -28,6 +32,16 @@ class MainActivity : AppCompatActivity() {
 
         redirectFetchDestinationBtn.setOnClickListener {
             val intent = Intent(this, FetchDestinationActivity::class.java)
+            startActivity(intent)
+        }
+
+        redirectLoginBtn.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
+
+        redirectSignUpBtn.setOnClickListener {
+            val intent = Intent(this, SignUpActivity::class.java)
             startActivity(intent)
         }
     }

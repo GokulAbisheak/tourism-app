f67d8297934789cd5574e72b8572724bbcad57c8
IT21375132 <gokulabisheak12@gmail.com>
2023-05-11 03:22:04 +0530

    Integration 01

---
+++ app/src/main/java/com/example/tourism_app/activities/LocationDetailsActivity.kt
@@ -0,0 +1,137 @@
+package com.example.tourism_app.activities
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import com.example.tourism_app.R
+import com.example.tourism_app.models.CustomerModel
+import com.google.firebase.database.FirebaseDatabase
+
+class LocationDetailsActivity : AppCompatActivity() {
+
+    private lateinit var tvCusId: TextView
+    private lateinit var tvmyName: TextView
+    private lateinit var tvmyPhone: TextView
+    private lateinit var tvmyRent: TextView
+    private lateinit var tvmyAddress: TextView
+    private lateinit var btnUpdate: Button
+    private lateinit var btnDelete: Button
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_location_details)
+
+        initView()
+        setValuesToViews()
+
+        btnUpdate.setOnClickListener {
+            openUpdateDialog(
+                intent.getStringExtra("CusId").toString(),
+                intent.getStringExtra("myName").toString()
+
+            )
+        }
+        btnDelete.setOnClickListener{
+            deleteRecord(
+                intent.getStringExtra("CusId").toString()
+            )
+        }
+    }
+    private fun deleteRecord(CusId:String){
+        val dbRef =FirebaseDatabase.getInstance().getReference("MyDetails").child(CusId)
+        val mTask =dbRef.removeValue()
+
+        mTask.addOnSuccessListener {
+            Toast.makeText(this,"Hotel data deleted", Toast.LENGTH_LONG).show()
+            val intent= Intent(this,FetchLocation::class.java)
+            finish()
+            startActivity(intent)
+        }.addOnFailureListener{error ->
+            Toast.makeText(this,"Deleting Err ${error.message}", Toast.LENGTH_LONG).show()
+        }
+    }
+    private fun initView(){
+        tvCusId = findViewById(R.id.tvCusId)
+        tvmyName = findViewById(R.id.tv_my_name)
+        tvmyPhone = findViewById(R.id.tv_my_phone)
+        tvmyRent = findViewById(R.id.tv_my_Rent)
+        tvmyAddress = findViewById(R.id.tv_my_Address)
+        btnUpdate = findViewById(R.id.update_button)
+        btnDelete = findViewById(R.id.delete_button)
+
+    }
+    private fun setValuesToViews(){
+
+        tvCusId.text=intent.getStringExtra("CusId")
+        tvmyName.text=intent.getStringExtra("myName")
+        tvmyPhone.text=intent.getStringExtra("myPhone")
+        tvmyRent.text=intent.getStringExtra("myRent")
+        tvmyAddress.text=intent.getStringExtra("myAddress")
+
+    }
+
+    private fun openUpdateDialog(
+        CusId: String,
+        myName:String
+    )
+    {
+        val mDialog = AlertDialog.Builder(this)
+        val inflater=layoutInflater
+        val mDialogView=inflater.inflate(R.layout.update_dialog,null)
+
+        mDialog.setView(mDialogView)
+
+        val et_myName = mDialogView.findViewById<EditText>(R.id.upmyName)
+        val et_myPhone = mDialogView.findViewById<EditText>(R.id.upmyPhone)
+        val et_myRent = mDialogView.findViewById<EditText>(R.id.upmyRent)
+        val et_myAddress = mDialogView.findViewById<EditText>(R.id.upmyAddress)
+        val btnUpdateData = mDialogView.findViewById<Button>(R.id.btnUpdateData)
+
+        et_myName.setText(intent.getStringExtra("myName").toString())
+        et_myPhone.setText(intent.getStringExtra("myPhone").toString())
+        et_myRent.setText(intent.getStringExtra("myRent").toString())
+        et_myAddress.setText(intent.getStringExtra("myAddress").toString())
+
+        mDialog.setTitle("Updating $myName Record")
+
+        val alertDialog=mDialog.create()
+        alertDialog.show()
+
+        btnUpdateData.setOnClickListener {
+            updateLocationData(
+                CusId,
+                et_myName.text.toString(),
+                et_myPhone.text.toString(),
+                et_myRent.text.toString(),
+                et_myAddress.text.toString()
+
+            )
+            Toast.makeText(applicationContext,"Hotel Data Updated",Toast.LENGTH_LONG).show()
+
+            //Update Data to our txt view
+            tvmyName.text=et_myName.text.toString()
+            tvmyPhone.text=et_myPhone.text.toString()
+            tvmyRent.text=et_myRent.text.toString()
+            tvmyAddress.text=et_myAddress.text.toString()
+
+            alertDialog.dismiss()
+        }
+    }
+    private fun updateLocationData(
+        CusId:String,
+        name:String,
+        phone:String,
+        rent:String,
+        address:String
+    ){
+        val dbRef =FirebaseDatabase.getInstance().getReference("MyDetails").child(CusId)
+        val locationInfo = CustomerModel(CusId,name,phone,rent,address)
+        dbRef.setValue(locationInfo)
+    }
+}
--- app/src/main/java/com/example/tourism_app/activities/MainActivity.kt
+++ app/src/main/java/com/example/tourism_app/activities/MainActivity.kt
@@ -1,48 +1,60 @@
 package com.example.tourism_app.activities
 
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
-import android.widget.Button
 import com.example.tourism_app.R
-import com.google.firebase.database.DatabaseReference
-import com.google.firebase.database.FirebaseDatabase
+import android.content.Intent
+import android.os.Bundle
+import android.widget.ImageButton
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var redirectAddDestinationBtn: Button
-    private lateinit var redirectFetchDestinationBtn: Button
-    private lateinit var redirectLoginBtn: Button
-    private lateinit var redirectSignUpBtn: Button
+    private lateinit var homeButton: ImageButton
+    private lateinit var destinationButton: ImageButton
+    private lateinit var vehicleButton: ImageButton
+    private lateinit var hotelButton: ImageButton
+    private lateinit var userButton: ImageButton
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        redirectAddDestinationBtn = findViewById(R.id.redirect_add_destination_button)
-        redirectFetchDestinationBtn = findViewById(R.id.redirect_fetch_destination_button)
-        redirectLoginBtn = findViewById(R.id.redirect_login_button)
-        redirectSignUpBtn = findViewById(R.id.redirect_signup_button)
+        destinationButton = findViewById(R.id.destination_nav)
+        vehicleButton = findViewById(R.id.vehicle_nav)
+        hotelButton = findViewById(R.id.hotel_nav)
+        userButton = findViewById(R.id.user_nav)
 
-        var firebase: DatabaseReference = FirebaseDatabase.getInstance().getReference()
+//        homeButton?.let { button ->
+//            button.setOnClickListener {
+//                val intent = Intent(this, HomeActivity::class.java)
+//                startActivity(intent)
+//            }
+//        }
 
-        redirectAddDestinationBtn.setOnClickListener {
-            val intent = Intent(this, AddDestinationActivity::class.java)
-            startActivity(intent)
-        }
+//        homeButton.setOnClickListener {
+//            val intent = Intent(this, HomeActivity::class.java)
+//            startActivity(intent)
+//        }
 
-        redirectFetchDestinationBtn.setOnClickListener {
+        destinationButton.setOnClickListener {
             val intent = Intent(this, FetchDestinationActivity::class.java)
             startActivity(intent)
         }
 
-        redirectLoginBtn.setOnClickListener {
-            val intent = Intent(this, LoginActivity::class.java)
+//        vehicleButton.setOnClickListener {
+//            val intent = Intent(this, VehicleActivity::class.java)
+//            startActivity(intent)
+//        }
+
+        hotelButton.setOnClickListener {
+            val intent = Intent(this, FetchLocation::class.java)
             startActivity(intent)
         }
 
-        redirectSignUpBtn.setOnClickListener {
-            val intent = Intent(this, SignUpActivity::class.java)
-            startActivity(intent)
-        }
+//        userButton.setOnClickListener {
+//            val intent = Intent(this, UserActivity::class.java)
+//            startActivity(intent)
+//        }
     }
 }

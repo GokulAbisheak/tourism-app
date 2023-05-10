1421b136b7739557e86ab466dbb36752a72ab47e
Sankeethan Y <99704338+IT21228162@users.noreply.github.com>
2023-05-10 20:30:20 +0530

    Add files via upload

---
+++ app/src/main/java/com/example/tourism_app/models/CustomerModel.kt
@@ -0,0 +1,10 @@
+package com.example.touristregister.models
+
+data class CustomerModel (
+    var CusId:String? = null,
+    var name:String? = null,
+    var phone:String? = null,
+    var rent:String? = null,
+    var address:String? = null
+
+)
